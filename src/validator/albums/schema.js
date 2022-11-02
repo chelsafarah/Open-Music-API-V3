ddaa7cbf801de422fb8ed184d6bfebdd77601139
unknown <chelsafv75@gmail.com>
2022-11-02 18:59:52 +0700

    add api for upload album cover

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -4,6 +4,7 @@ const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
   year: Joi.number().integer().min(1900).max(2022)
     .required(),
+  coverUrl: Joi.string(),
 });
 
 module.exports = { AlbumPayloadSchema };
